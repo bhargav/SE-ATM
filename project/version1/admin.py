748ae65e6f4b805d8e9ead8b30450a250713bb8f
Parteek Singla <parteekiitp@gmail.com>
2011-11-04 00:42:30 +0530

    atmrefill class defined

--- project/version1/admin.py
+++ project/version1/admin.py
@@ -1,4 +1,6 @@
 from version1.models import Machine
+from version1.models import MachineRefill
 from django.contrib import admin
 
 admin.site.register(Machine)
+admin.site.register(MachineRefill)
--- project/version1/models.py
+++ project/version1/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Machine(models.Model):
-	machine_id = models.IntegerField()
-	location = models.CharField(max_length=200)
-	minimum_atm_balance = models.IntegerField()
-	current_balance = models.IntegerField()
+	machine_id = models.IntegerField("MACHINE ID",primary_key=True)
+	location = models.CharField("LOCATION",max_length=200)
+	minimum_atm_balance = models.DecimalField("MINIMUM ATM BALANCE",decimal_places=2,max_digits=10)
+	current_balance = models.DecimalField("BALANCE",decimal_places=2,max_digits=10)
 	last_refill_date = models.DateTimeField('LAST REFILL DATE')
 	next_maintainence_date = models.DateTimeField('NEXT MAINTAINENCE DATE')
+
+
+class MachineRefill(models.Model):
+	refill_id = models.IntegerField("REFILL ID",primary_key=True)
+	machine_id = models.ForeignKey(Machine)
+	refill_date = models.DateTimeField('REFILL DATE')
+	previous_balance = models. DecimalField("PREVIOUS ATM BALANCE",decimal_places=2,max_digits=10)
+	amount_refilled = models.DecimalField("AMOUNT REFILLED",decimal_places=2,max_digits=10)
+	
