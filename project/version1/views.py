f53644b9d5181302257c1c90adc61bae04057108
Bhargav Mangipudi <bhargav.1191@gmail.com>
2011-11-11 00:21:15 +0530

    update session variable to hold only the card number, so that changes made in the database are reflected each time the page is refreshed

--- project/version1/views.py
+++ project/version1/views.py
@@ -8,7 +8,6 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404 
 import datetime
 
-session=0
 def index(request):
     Account_holder_list = Account_Ext.objects.all()
     return render_to_response('version1/index.html', locals())
@@ -16,24 +15,24 @@ def index(request):
 def validatecard(request):
 	global session
 	p = get_object_or_404(Account_Ext, pk=request.GET['cardnumber'])
-	session = p
+	session = request.GET['cardnumber']
 	return render_to_response('version1/pincode.html', locals())
     
 def validatepin(request):
     #Account_holder_list = Account_Ext.objects.all()
     global session
-    pin = ATM_Card.objects.filter(pin=request.GET['pincode'],atmcard_num=session.atmcard_num)
+    pin = ATM_Card.objects.filter(pin=request.GET['pincode'],atmcard_num=session)
     if pin:
 		return render_to_response('version1/options.html', locals())	
     return HttpResponse("Your pincode is not valid")
 
 def balanceenquiry(request):
 	global session
-	#ac = Account_Ext.objects.filter(atmcard_num=session.atmcard_num)
+	[t_acc] = Account_Ext.objects.filter(atmcard_num=session)
 	#mach = Machine.objects.filter(machine_id =1)
-	t = Balance_Enquiry(atmcard_num_id = session.atmcard_num, machine_id_id = 1,tid = 1,date_time = datetime.datetime.now(),status = "Completed",rescode = 1,type_trans = "Balance Enquiry",bal_amount=session.balance)
+	t = Balance_Enquiry(atmcard_num_id = session, machine_id_id = 1,tid = 1,date_time = datetime.datetime.now(),status = "Completed",rescode = 1,type_trans = "Balance Enquiry",bal_amount=t_acc.balance)
 	t.save()
-	bal = session.balance
+	bal = t_acc.balance
 	return render_to_response('version1/balance.html', locals())
 	
 def cashwithdrawal(request):
